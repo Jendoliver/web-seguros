122e0863d18025849bbab0417311e8a830db5002
Jhen D'Òliver Morious <jandol1996@hotmail.com>
2017-05-01 20:20:36 +0000

    This closes #3 - Contact form added. Working on #5

--- js/xxxsmoothiexxx.js
+++ js/xxxsmoothiexxx.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
   // Initialize Tooltip
   $('[data-toggle="tooltip"]').tooltip(); 
-  
+  $("body").attr("data-offset", $('.navbar').height());
   // Add smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
@@ -19,7 +19,7 @@ $(document).ready(function(){
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top - $('.navbar').height() // Dynamic offset calculation
       }, 900, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
